967addcba70b7ea963b96df6ba26a269ec3b5f9d
DJ-GUIGUI <95770718+DJ-GUIGUI@users.noreply.github.com>
2021-12-08 19:02:40 +0100

    v1.0.0

--- page_template.php
+++ page_template.php
@@ -1,33 +1,37 @@
-<body>
-	<!-- Haut de Page -->
-	<header style="text-align: center;">
-		<img src="multimedia/image/logo.png" width="80">
-		<h1>
-			DJ GUIGUI
-		</h1>
-		<p>
-			Slogan pas encore définis poposez nous en un par mail le lien est dans <a href="HTML/contact.html">Contact</a>
-		</p>
-	</header>
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<!-- Haut de Page -->
+		<header style="text-align: center;">
+			<img src="multimedia/image/logo.png" width="80">
+			<h1>
+				DJ GUIGUI
+			</h1>
+			<p>
+				Slogan pas encore définis poposez nous en un par mail le lien est dans <a href="HTML/contact.html">Contact</a>
+			</p>
+		</header>
 
-	<!-- Menu de Navigation -->
-	<nav class="menu-nav" style="text-align: center;">
-		<ul>
-			<li class="btn">
-				<a href="index.html">
-					Accueil
-				</a>
-			</li>
-			<li class="btn">
-				<a href="HTML/a propos.html">
-					A propos
-				</a>
-			</li>
-			<li class="btn">
-				<a href="HTML/contact.html">
-					Contact
-				</a>
-			</li>
-		</ul>
-	</nav>
-</body>
+		<!-- Menu de Navigation -->
+		<nav class="menu-nav" style="text-align: center;">
+			<ul>
+				<li class="btn">
+					<a href="index.html">
+						Accueil
+					</a>
+				</li>
+				<li class="btn">
+					<a href="HTML/a propos.html">
+						A propos
+					</a>
+				</li>
+				<li class="btn">
+					<a href="HTML/contact.html">
+						Contact
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</body>
+</html>
