b00917d7cf365b965c14825c4b71ff57df7417a6
DJ-GUIGUI <95770718+DJ-GUIGUI@users.noreply.github.com>
2021-12-08 19:00:25 +0100

    v1.0.0

---
+++ page_template.php
@@ -0,0 +1,33 @@
+<body>
+	<!-- Haut de Page -->
+	<header style="text-align: center;">
+		<img src="multimedia/image/logo.png" width="80">
+		<h1>
+			DJ GUIGUI
+		</h1>
+		<p>
+			Slogan pas encore définis poposez nous en un par mail le lien est dans <a href="HTML/contact.html">Contact</a>
+		</p>
+	</header>
+
+	<!-- Menu de Navigation -->
+	<nav class="menu-nav" style="text-align: center;">
+		<ul>
+			<li class="btn">
+				<a href="index.html">
+					Accueil
+				</a>
+			</li>
+			<li class="btn">
+				<a href="HTML/a propos.html">
+					A propos
+				</a>
+			</li>
+			<li class="btn">
+				<a href="HTML/contact.html">
+					Contact
+				</a>
+			</li>
+		</ul>
+	</nav>
+</body>
