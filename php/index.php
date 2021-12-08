81ad5eb3685090a42a97a985ccf927e21c4a037a
DJ-GUIGUI <95770718+DJ-GUIGUI@users.noreply.github.com>
2021-12-08 19:21:40 +0100

    v1.0.0

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<?php require('index.html')?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Titre</title>
+</head>
+<body>
+	<header>
+		<!-- Menu de Navigation -->
+		<?php include 'menunavigation.php'; ?>
+	</header>
+</body>
+</html>
